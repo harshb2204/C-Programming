1015c9243b56b8a5dedde5c339c6f880264f8db9
Harsh <harshb2204@gmail.com>
2025-02-15 07:45:27 +0000

    variables, data types

---
+++ src/c/HelloWorld/Hello_World.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  // Standard Input/Output library, required for printf function
+
+// The main function - execution of the program starts from here
+int main() {
+    printf("Hello world");  // Prints "Hello world" to the console
+
+    return 0;  // Returns 0 to indicate successful execution
+}
