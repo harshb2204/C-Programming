b10e28261f21c87b923ee84394c6070d7a610f05
Harsh <harshb2204@gmail.com>
2025-02-15 12:57:37 +0000

    operators

---
+++ src/c/Numbers/Numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 5;
+
+    // Addition
+    printf("Addition: %d + %d = %d\n", a, b, a + b);
+
+    // Subtraction
+    printf("Subtraction: %d - %d = %d\n", a, b, a - b);
+
+    // Multiplication
+    printf("Multiplication: %d * %d = %d\n", a, b, a * b);
+
+    // Division
+    printf("Division: %d / %d = %d\n", a, b, a / b);
+
+    // Modulus (remainder)
+    printf("Modulus: %d %% %d = %d\n", a, b, a % b);
+
+    // Increment
+    printf("Increment: %d++ = %d\n", a, ++a);
+
+    // Decrement
+    printf("Decrement: %d-- = %d\n", b, --b);
+
+    return 0;
+}
